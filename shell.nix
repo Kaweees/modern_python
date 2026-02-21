583763e0b99e7c428be22fba06074f981e72a8f5
Miguel Villa Floran <miguel.villafloran@gmail.com>
2026-02-20 16:37:50 -0800

    Update

--- shell.nix
+++ shell.nix
@@ -2,20 +2,21 @@
   pkgs ? import <nixpkgs> { },
 }:
 
+let
+  python = pkgs.python312;
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    python312 # Python 3.12
-    uv # Python package manager
-    nixfmt # Nix formatter
-    just # Just
-    stdenv.cc.cc.lib # C++ standard library
+    python
+    uv
+    nixfmt
+    just
   ];
 
   # Shell hook to set up environment
   shellHook = ''
     export TMPDIR=/tmp
-    export UV_PYTHON="${pkgs.python312}/bin/python"
-    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+    export UV_PYTHON="${python}/bin/python"
     just install
   '';
 }
